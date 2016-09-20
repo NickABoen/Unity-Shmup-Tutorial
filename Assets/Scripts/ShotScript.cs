1e337ce4eb6ef62614c6804d13184cc582f80b87
NickABoen <NickABoen@gmail.com>
2016-09-20 01:21:58 -0500

    Implement the script that defines bullets

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Projectile behaviour
+/// </summary>
 public class ShotScript : MonoBehaviour {
+    // 1 - Designer variables
+
+    /// <summary>
+    /// Damage inflicted
+    /// </summary>
+    public int damage = 1;
+
+    /// <summary>
+    /// Projectile damage player or enemies?
+    /// </summary>
+    public bool isEnemyShot = false;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        // 2 - Limited time to live to avoid any leak
+        Destroy(gameObject, 20); // 20sec
 	}
 }
