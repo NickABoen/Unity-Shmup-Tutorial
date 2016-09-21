59e1aa371345019ad2016633f44b35656124398b
NickABoen <NickABoen@gmail.com>
2016-09-20 22:45:47 -0500

    Create and implement enemy script with basic AI

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour {
+    private WeaponScript weapon;
+
+    void Awake()
+    {
+        //Retrieve the weapon only once
+        weapon = GetComponent<WeaponScript>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    // Auto-fire
+        if(weapon!=null && weapon.CanAttack)
+        {
+            weapon.Attack(true);
+        }
+	}
+}
