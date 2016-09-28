bb74a571337652c65d467469804ce81ad4f28d3e
NickABoen <NickABoen@gmail.com>
2016-09-28 00:36:39 -0500

    Make special effects script to do explosions

---
+++ Assets/Scripts/SpecialEffectsHelper.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Creating instance of particles from code with no effort
+/// </summary>
+public class SpecialEffectsHelper : MonoBehaviour {
+
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static SpecialEffectsHelper Instance;
+
+    public ParticleSystem smokeEffect;
+    public ParticleSystem fireEffect;
+
+    void Awake()
+    {
+        //Register the singleton
+        if(Instance != null)
+        {
+            Debug.LogError("Multiple instances of SpecialEffectsHelper!");
+        }
+
+        Instance = this;
+    }
+
+    /// <summary>
+    /// Create an explosion at the given location
+    /// </summary>
+    ///<param name="position">Center of explosion</param>
+    public void Explosion(Vector3 position)
+    {
+        //Smoke on the water
+        instantiate(smokeEffect, position);
+
+        // Tu tu tu, tu tu tudu
+
+        //Fire in the sky
+        instantiate(fireEffect, position);
+    }
+
+    /// <summary>
+    /// Instantiate a Particle system from prefab
+    /// </summary>
+    /// <param name="prefab">Particle system to instantiate</param>
+    /// <param name="position">position to instantiate it to</param>
+    /// <returns>Instantiated particle system</returns>
+    private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position)
+    {
+        ParticleSystem newParticleSystem = Instantiate(prefab, position, Quaternion.identity) as ParticleSystem;
+
+        //Make sure it will be destroyed
+        Destroy(newParticleSystem.gameObject, newParticleSystem.startLifetime);
+
+        return newParticleSystem;
+    }
+}
