dce17bf3e8805e92629e3012fa3ae3e650265b21
NickABoen <NickABoen@gmail.com>
2016-09-28 01:19:56 -0500

    Creat script to manage and play sounds when needed

---
+++ Assets/Scripts/SoundEffectsHelper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Creating instance of sounds from code with no effort
+/// </summary>
+public class SoundEffectsHelper : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static SoundEffectsHelper Instance;
+
+    public AudioClip explosionSound;
+    public AudioClip playerShotSound;
+    public AudioClip enemyShotSound;
+
+    void Awake()
+    {
+        //Register the singleton
+        if(Instance != null)
+        {
+            Debug.LogError("Multiple instances of SoundEffectsHelper!");
+        }
+        Instance = this;
+    }
+
+    public void MakeExplosionSound()
+    {
+        MakeSound(explosionSound);
+    }
+
+    public void MakePlayerShotSound()
+    {
+        MakeSound(playerShotSound);
+    }
+
+    public void MakeEnemyShotSound()
+    {
+        MakeSound(enemyShotSound);
+    }
+
+    /// <summary>
+    /// Play a given sounud
+    /// </summary>
+    /// <param name="originalClip">Sound clip to play</param>
+    private void MakeSound(AudioClip originalClip)
+    {
+        // As it is not 3D audio clip, position doesn't matter.
+        AudioSource.PlayClipAtPoint(originalClip, transform.position);
+    }
+}
