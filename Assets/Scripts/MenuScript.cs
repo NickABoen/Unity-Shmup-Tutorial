7f5aae9c4ca1145b9cfb255fda38ba635abd6d63
NickABoen <NickABoen@gmail.com>
2016-09-28 22:55:28 -0500

    Add play button and menu script for starting game

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour {
+    public void StartGame()
+    {
+        // "Stage1" is the name of the first scene we created.
+        Application.LoadLevel("Stage1");
+    }
+}
